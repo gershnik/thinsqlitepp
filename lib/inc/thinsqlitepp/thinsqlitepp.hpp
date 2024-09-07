8df03cc1ebe08e0e4df08249b51620175505d577
Eugene Gershnik <gershnik@users.noreply.github.com>
2024-09-06 22:59:54 -0700

    Adding an umbrella header

---
+++ lib/inc/thinsqlitepp/thinsqlitepp.hpp
@@ -0,0 +1,20 @@
+/*
+ Copyright 2019 Eugene Gershnik
+
+ Use of this source code is governed by a BSD-style
+ license that can be found in the LICENSE file or at
+ https://github.com/gershnik/thinsqlitepp/blob/main/LICENSE
+*/
+
+#ifndef HEADER_SQLITEPP_SQLITEPP_INCLUDED
+#define HEADER_SQLITEPP_SQLITEPP_INCLUDED
+
+#include <thinsqlitepp/context.hpp>
+#include <thinsqlitepp/database.hpp>
+#include <thinsqlitepp/exception.hpp>
+#include <thinsqlitepp/global.hpp>
+#include <thinsqlitepp/mutex.hpp>
+#include <thinsqlitepp/statement.hpp>
+#include <thinsqlitepp/value.hpp>
+
+#endif 
