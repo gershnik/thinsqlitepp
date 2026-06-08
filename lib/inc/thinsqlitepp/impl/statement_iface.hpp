869c2fe6692593e68da10999ddd01da7d9fe817f
Eugene Gershnik <gershnik@users.noreply.github.com>
2026-06-08 00:19:26 -0700

    Fixing license links

--- lib/inc/thinsqlitepp/impl/statement_iface.hpp
+++ lib/inc/thinsqlitepp/impl/statement_iface.hpp
@@ -3,7 +3,7 @@
 
  Use of this source code is governed by a BSD-style
  license that can be found in the LICENSE file or at
- https://github.com/gershnik/thinsqlitepp/blob/main/LICENSE
+ https://github.com/gershnik/thinsqlitepp/blob/master/LICENSE
 */
 
 #ifndef HEADER_SQLITEPP_STATEMENT_IFACE_INCLUDED
